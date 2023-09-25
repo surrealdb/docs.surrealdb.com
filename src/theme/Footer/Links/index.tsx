c000c37849cc17a5bec8d9ad60582945f85867fa
Dimitri <dimitrianoudi@gmail.com>
2023-09-25 15:55:02 +0300

    feat(multi-instance): Adds multi-instance docs and replaces correct f… (#4)
    * feat(multi-instance): Adds multi-instance docs and replaces correct footer links
    
    * feat(multi-instance): Fixes multi-instance tree folders and config
    
    * feat(multi-instance): Adds multi instance and new styling
    
    * feat(multi-instance): adds layout items
    
    * feat(multi-instance): fixes h4 styling
    
    * feat(multi-instance): Syntax highlighting amendments
    
    * feat(multi-instance): Removes syntax hightliting colour on hover
    
    * feat(multi-instance): Docs sub-pages menu
    
    * feat(multi-instance): Fixes menu links hover colour
    
    * feat(multi-instance): Removes unecessary code
    
    * feat(multi-instance): Removes multi versions dropdown
    
    ---------
    
    Co-authored-by: dimitri.anoudis <dimitri.anoudis@news.co.uk>

--- src/theme/Footer/Links/index.tsx
+++ src/theme/Footer/Links/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import {isMultiColumnFooterLinks} from '@docusaurus/theme-common';
-import FooterLinksMultiColumn from '@theme/Footer/Links/MultiColumn';
-import FooterLinksSimple from '@theme/Footer/Links/Simple';
+import FooterLinksMultiColumn from '@theme-original/Footer/Links/MultiColumn';
+import FooterLinksSimple from '@theme-original/Footer/Links/Simple';
 import type {Props} from '@theme/Footer/Links';
 
 export default function FooterLinks({links}: Props): JSX.Element {
