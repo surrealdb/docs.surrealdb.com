3b6198dc78db5cd777f6993a51a761fe367603d9
Micha de Vries <micha@devrie.sh>
2023-10-13 20:43:47 +0200

    UI & UX improvements

--- src/theme/SearchBar/index.tsx
+++ src/theme/SearchBar/index.tsx
@@ -1,10 +1,10 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styles from './styles.module.css';
 import { Doc, search } from './search';
 
 
 function SearchBar(): JSX.Element | null {
-    const dialog = useRef<HTMLDialogElement | null>(null);
+    const dialogRef = useRef<HTMLDialogElement>();
     const [keywords, setKeywords] = useState('');
     const [results, setResults] = useState<Doc[]>([]);
 
@@ -23,41 +23,53 @@ function SearchBar(): JSX.Element | null {
                 console.error(err);
             }
         }
-      }, [dialog]);
+    }, [dialogRef]);
+
+    useEffect(() => {
+        const handler = (e) => {
+            if (dialogRef.current && !dialogRef.current.contains(e.target))
+                dialogRef.current.close();
+        };
+
+        document.addEventListener("mousedown", handler);
+        return () => document.removeEventListener("mousedown", handler);
+    }, [dialogRef]);
 
     return (
         <>
-            <a className='navbar__link' aria-label="Search" onClick={(e)=>dialog.current.showModal()}>
-                <svg width="20" height="20" viewBox='0 -4 24 24'>
+            <a className={styles.link + ' navbar__link'} aria-label="Search" onClick={()=>dialogRef.current.showModal()}>
+                <svg className={styles.linkicon} width="20" height="20" viewBox='0 -4 24 24'>
                     <path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path>
                 </svg>
                 Search
             </a>
-            <dialog className={styles.modal} ref={dialog}>
+            <dialog className={styles.modal} ref={dialogRef}>
                 <form method="dialog">
                     <input className={styles.input} onChange={(e)=>handleChange(e.target.value)} type="text" placeholder="Search.." />
                 </form>
-                {results && results.length > 0 &&
-                    results.map((doc)=>{
-                        return (
-                    <div className={styles.result} key={doc.url}>
-                        <a href={doc.url}>
-                            <h4 className={styles.title}>{doc.title}</h4>
-                            <h6 className={styles.url}>{doc.url}</h6>
-                            <p className={styles.match}>
-                            { doc.chunks && doc.chunks.length > 0 && doc.chunks.map((ch, idx) => {
-                                    if (ch.bold) {
-                                        return (<b key={idx}>{ch.value} </b>);
-                                    } else {
-                                        return (<span key={idx}>{ch.value} </span>);
-                                    }
-                                }
-                                )
-                            }
-                            </p>
-                        </a>
-                        </div>
-                       )})}
+                {(results && results.length > 0) ? (
+                    <div className={styles.results}>
+                        {results.map((doc)=>(
+                            <a href={doc.url} className={styles.result} key={doc.url}>
+                                <h4 className={styles.title}>{doc.title}</h4>
+                                <h6 className={styles.url}>{doc.url}</h6>
+                                <p className={styles.match}>
+                                    {doc.chunks && doc.chunks.length > 0 && doc.chunks.map((ch, idx) => {
+                                        if (ch.bold) {
+                                            return (<b key={idx}>{ch.value} </b>);
+                                        } else {
+                                            return (<span key={idx}>{ch.value} </span>);
+                                        }
+                                    })}
+                                </p>
+                            </a>
+                        ))}
+                    </div>
+                ) : (
+                    <div className={styles.empty}>
+                        <p>{keywords == '' ? 'Enter a search term' : 'No results'}</p>
+                    </div>
+                )}
             </dialog>
         </>
     );
