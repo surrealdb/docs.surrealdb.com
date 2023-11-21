185169627d71ad2990b4c321544fef8e2e389866
Micha de Vries <micha@devrie.sh>
2023-11-21 10:35:14 +0100

    use node-fetch

--- plugins/crawler/index.ts
+++ plugins/crawler/index.ts
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 export const onSuccess = async function() {
     console.log(process.env.DEPLOY_URL);
     console.log('can fetch: ' + (await fetch(process.env.DEPLOY_URL)).status);
