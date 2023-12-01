4689fbbd32bf55a0129e79da339bb818a0350d95
Dimitri <dimitrianoudi@gmail.com>
2023-12-01 15:16:58 +0000

    feat(various-fixes): Adds FontAwesome and new menu icon (#84)

---
+++ src/theme/Icon/Menu/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function IconMenu() {
+  return (
+    <FontAwesomeIcon icon={faBars} />
+  );
+}
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,6 +2,12 @@ import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 import Highlight from '@site/src/components/Highlight';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import the FontAwesomeIcon component
+import { library } from '@fortawesome/fontawesome-svg-core'; // Import the library component
+import { fab } from '@fortawesome/free-brands-svg-icons'; // Import all brands icons
+import { fas } from '@fortawesome/free-solid-svg-icons'; // Import all solid icons
+
+library.add(fab, fas);
 
 export default {
   // Re-use the default mapping
@@ -9,4 +15,5 @@ export default {
   // Map the "<Highlight>" tag to our Highlight component
   // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
   Highlight,
+  Icon: FontAwesomeIcon,
 };
