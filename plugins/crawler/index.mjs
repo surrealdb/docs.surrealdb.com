c580fb7bcc72ec1db252ebe86284887e964e2474
Micha de Vries <micha@devrie.sh>
2023-12-05 12:00:01 +0100

    do index on prod

--- plugins/crawler/index.mjs
+++ plugins/crawler/index.mjs
@@ -5,7 +5,8 @@ import { Surreal } from "surrealdb.js";
 import { cwd } from "process";
 
 export async function onSuccess() {
-    const applyIndexes = process.env.DEPLOY_URL == 'https://main--surrealdb-docs.netlify.app/';
+    // const isLocalBuild = process.env.DEPLOY_URL == 'https://0--surrealdb-docs.netlify.app';
+    const applyIndexes = process.env.DEPLOY_PRIME_URL == 'https://main--surrealdb-docs.netlify.app/';
     const jobDate = new Date();
     const db = new Surreal({
         onConnect: () => console.log("[DB] Connected to SurrealDB"),
