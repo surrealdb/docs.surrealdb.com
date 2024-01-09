6acf53e11d69d062da6abdb729f0d6ddea36d9cf
Dimitri Anoudis <dimitrianoudi@gmail.com>
2024-01-09 14:36:56 +0200

    feat(giscus): Adds Giscus to docs

---
+++ src/components/GiscusComponent/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Giscus from "@giscus/react";
+import { useColorMode } from '@docusaurus/theme-common';
+
+export default function GiscusComponent() {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Giscus    
+      repo="surrealdb/docs.surrealdb.com"
+      repoId="MDEwOlJlcG9zaXRvcnkyMjc4MTY1NzM="
+      category="General"
+      categoryId="IdOfDiscussionCategory" //id of the category above (General)
+      mapping="url"                      // Important! To map comments to URL
+      term="Welcome to @giscus/react component!"
+      strict="0"
+      reactionsEnabled="1"
+      emitMetadata="1"
+      inputPosition="top"
+      theme={colorMode}
+      lang="en"
+      loading="lazy"
+      crossorigin="anonymous"
+      async
+    />
+  );
+}
