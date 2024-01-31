e0bf37eed1e2fd3318d9694853d3df089fb61d14
Dimitri Anoudis <dimitrianoudi@gmail.com>
2024-01-31 17:41:08 +0000

    feat(404page): It redirects to last known path

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import NotFound from '@theme-original/NotFound';
+import { useLocation } from '@docusaurus/router';
+import { Redirect } from '@docusaurus/router';
 
-export default function NotFoundWrapper(props) {
-  return (
-    <>
-      <NotFound {...props} />
-    </>
-  );
+export default function NotFound() {
+  
+  const location = useLocation();
+  const { pathname } = location;
+  const lastSlashIndex = pathname.lastIndexOf('/');
+  const redirectTo = pathname.slice(0, lastSlashIndex);
+
+  return <Redirect to={redirectTo} />;
 }
