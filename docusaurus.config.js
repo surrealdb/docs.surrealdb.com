2c1d927f9879c9821c902fa9d89b0455bdf7e021
Micha de Vries <micha@devrie.sh>
2024-02-28 22:29:23 +0100

    always build to build/docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const config = {
   url: 'https://surrealdb.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: isProd ? '/docs/' : '/',
+  baseUrl: '/docs/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'SurrealDB', // Usually your GitHub org/user name.
