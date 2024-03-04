94223c2938d02fd34f8818b44ac83d31e578c5e7
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-03-04 07:49:56 +0000

    Remove unnecessary comment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,6 @@ const config = {
   // Set the production url of your site here
   url: 'https://surrealdb.com',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs/',
   // TODO We need to fix these issues, just not doing it now :)
   onBrokenLinks: 'warn',
