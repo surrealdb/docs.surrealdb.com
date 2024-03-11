d5ae41465564482a7a961e6c9271f211baf24d39
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-03-11 12:30:34 +0000

    Upgrade to Docusaurus 3

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,8 +1,6 @@
-import React from 'react';
-// Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
-import Highlight from '@site/src/components/Highlight';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import the FontAwesomeIcon component
+import Version from '@site/src/components/Version';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'; // Import the FontAwesomeIcon component
 import { library } from '@fortawesome/fontawesome-svg-core'; // Import the library component
 import { fab } from '@fortawesome/free-brands-svg-icons'; // Import all brands icons
 import { fas } from '@fortawesome/free-solid-svg-icons'; // Import all solid icons
@@ -12,8 +10,6 @@ library.add(fab, fas);
 export default {
   // Re-use the default mapping
   ...MDXComponents,
-  // Map the "<Highlight>" tag to our Highlight component
-  // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
-  Highlight,
-  Icon: FontAwesomeIcon,
+  Version,
+  Icon,
 };
