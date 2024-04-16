c3d682e07ed3fdf94d2afe419b0c6abf4100c693
Dimitri Anoudis <dimitrianoudi@gmail.com>
2024-04-16 14:55:29 +0100

    doc-139(remove-url-hash): It removes the url hash on cases we dont want

---
+++ src/utils/ModifyHash.js
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { useLocation, useHistory } from '@docusaurus/router';
+
+function ModifyHash() {
+    const location = useLocation();
+    const history = useHistory();
+
+    useEffect(() => {
+        let modified = false;
+        let newHash = location.hash;
+        const substringsToRemove = ["-websocket-only", "-since-110", "-since-120", "-since-130", "-since-140"];
+
+        substringsToRemove.forEach(substring => {
+            const regex = new RegExp(substring, 'g');
+            if (newHash.includes(substring)) {
+                newHash = newHash.replace(regex, "");
+                modified = true;
+            }
+        });
+        
+
+        if (modified) {
+            history.replace({ ...location, hash: newHash });
+        }
+    }, [location, history]);
+
+    return null;
+}
+
+export default ModifyHash;
