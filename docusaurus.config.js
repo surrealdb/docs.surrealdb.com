4f3398ae506220d43dc2054050b58718e4972b5c
ekwuno <Obinnacodes@gmail.com>
2024-06-11 12:21:20 +0100

    chore: Update Docusaurus config to include version 2.x (alpha)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,14 +39,14 @@ const config = {
         includeCurrentVersion: false,
         routeBasePath: '/surrealdb',
         lastVersion: '1.x',
-        onlyIncludeVersions: ['1.x'],
+        onlyIncludeVersions: ['1.x', '2.x'],
         versions: {
           "1.x": {
             label: '1.x',
             path: '',
           },
           "2.x": {
-            label: '2.x',
+            label: '2.x (alpha)',
             path: '2.x',
           },
         },
