3fd1a16bf04f9e923a12b316123c552a626c7ca3
Dimitri Anoudis <dimitrianoudi@gmail.com>
2024-07-23 17:07:46 +0100

    feat(DOC-208-add-since-component): It simplifies the component more

--- src/components/Since/index.tsx
+++ src/components/Since/index.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import styles from './since.module.css';
 
-const Versions: string[] = [
-  'v2.0.0', 'v1.5.4', 'v1.5.3', 'v1.5.2', 'v1.5.1', 'v1.5.0',
-  'v1.4.2', 'v1.4.0', 'v1.3.1', 'v1.3.0', 'v1.2.2', 'v1.2.1',
-  'v1.2.0', 'v1.1.1', 'v1.1.0', 'v1.0.2', 'v1.0.1', 'v1.0.0',
-  'v0.3.0', 'v0.2.0', 'v0.1.0'
-];
+type Version = `v${number}.${number}.${number}`;
 
 interface SinceProps {
-    v: string;
+    v: Version;
 }
 
 const Since: React.FC<SinceProps> = ({ v }) => {
-    if (!Versions.includes(v)) {
-        return <div>Error: Version {v} is not recognized.</div>;
-    }
 
-    const versionWithoutV = v.startsWith('v') ? v.slice(1) : v;
+    const version = v.startsWith('v') ? v.slice(1) : v;
 
-    return <div className={styles.Version}>Since {versionWithoutV}</div>;
+    return <div className="version">Since {version}</div>;
 };
 
 export default Since;
