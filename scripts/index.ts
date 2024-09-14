a955ee319a6707a36560a8d2422652cbe40b218a
Odonno <bottiau.david@laposte.net>
2024-09-14 15:31:32 +0200

    feat: use sync & async Rust fn from source code instead of the fuzz dictionary

--- scripts/index.ts
+++ scripts/index.ts
@@ -3,23 +3,64 @@ import * as cheerio from "cheerio";
 
 const repositoryName = "surrealdb/surrealdb";
 const branch = "main";
+const fncSourceCodeFilePath = "core/src/fnc/mod.rs";
 
 const detectMissingFunctions = async () => {
   console.log("Detecting missing functions...");
   console.log("--------------------------------------------------");
 
-  const fuzzRawFilePath = "sdk/fuzz/fuzz_targets/fuzz_executor.dict";
-
   const response = await fetch(
-    `https://raw.githubusercontent.com/${repositoryName}/${branch}/${fuzzRawFilePath}`
+    `https://raw.githubusercontent.com/${repositoryName}/${branch}/${fncSourceCodeFilePath}`
   );
   const result = await response.text();
 
-  const allExistingFunctions = result
-    .split("\n")
-    .map((line) => line.replaceAll('"', ""))
-    .filter((line) => line.endsWith("(") && line.includes("::"))
-    .map((line) => line.replace("(", ""));
+  let lineStartForSyncFns = -1;
+  let lineEndForSyncFns = -1;
+
+  let lineStartForAsyncFns = -1;
+  let lineEndForAsyncFns = -1;
+
+  const lines = result.split("\n");
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+
+    if (line.startsWith("pub fn synchronous(")) {
+      lineStartForSyncFns = i;
+      continue;
+    }
+    if (line.startsWith("pub async fn asynchronous(")) {
+      lineStartForAsyncFns = i;
+      continue;
+    }
+
+    if (line === "}") {
+      if (lineStartForSyncFns > -1 && lineEndForSyncFns === -1) {
+        lineEndForSyncFns = i;
+        continue;
+      }
+      if (lineStartForAsyncFns > -1 && lineEndForAsyncFns === -1) {
+        lineEndForAsyncFns = i;
+        continue;
+      }
+    }
+  }
+
+  const allExistingFunctions: string[] = [];
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+
+    if (
+      (lineStartForSyncFns <= i && i < lineEndForSyncFns) ||
+      (lineStartForAsyncFns <= i && i < lineEndForAsyncFns)
+    ) {
+      const regexResult = /"(.+)" =>/gi.exec(line);
+      if (regexResult) {
+        allExistingFunctions.push(regexResult[1]);
+      }
+    }
+  }
 
   const allDocumentedFunctions: string[] = [];
 
@@ -31,7 +72,12 @@ const detectMissingFunctions = async () => {
     const $ = cheerio.load(data);
 
     const functionNames = $('td[data-label="Function"]')
-      .map((_, el) => $(el).text().replace("()", "").replace(/\u200B/g, ""))
+      .map((_, el) =>
+        $(el)
+          .text()
+          .replace("()", "")
+          .replace(/\u200B/g, "")
+      )
       .get();
 
     allDocumentedFunctions.push(...functionNames);
