829a9d36bc97b32b361f8514f17395307b955e0d
Micha de Vries <micha@devrie.sh>
2024-09-17 12:34:20 +0100

    code quality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 import icon from 'astro-icon';
 // @ts-check
 import { defineConfig } from 'astro/config';
 import remarkCustomHeadingId from 'remark-custom-heading-id';
 import { rehypeCodemirrorPlugin } from './src/util/rehypeCodemirrorPlugin.mjs';
 import { rehypeNotesPlugin } from './src/util/rehypeNotesPlugin.mjs';
-import partytown from '@astrojs/partytown';
-import compress from 'astro-compress';
 
 const deployDomain = process.env.DEPLOY_DOMAIN ?? 'surrealdb.com';
 const site = `https://${deployDomain}`;
