527c12e74c17a06e1af42e279bd024fb813e51b8
Micha de Vries <micha@devrie.sh>
2024-10-08 10:55:06 +0200

    Reintroduce crawler

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,8 @@ import { rehypeCopyCodePlugin } from './src/util/rehypeCopyCodePlugin.mjs';
 import { autolinkConfig } from './src/util/rehypeHeadingsConfig';
 import { rehypeNotesPlugin } from './src/util/rehypeNotesPlugin.mjs';
 
+import sitemap from '@astrojs/sitemap';
+
 const deployDomain = process.env.DEPLOY_DOMAIN ?? 'surrealdb.com';
 const site = `https://${deployDomain}`;
 
@@ -23,18 +25,11 @@ export default defineConfig({
     base: '/docs',
     outDir: './dist/docs',
     trailingSlash: 'never',
-    integrations: [
-        mdx(),
-        solidJs({ devtools: true }),
-        icon(),
-        tailwind({
-            nesting: true,
-        }),
-        partytown(),
-        compress({
-            Image: false,
-        }),
-    ],
+    integrations: [mdx(), solidJs({ devtools: true }), icon(), tailwind({
+        nesting: true,
+    }), partytown(), compress({
+        Image: false,
+    }), sitemap()],
     markdown: {
         remarkPlugins: [remarkCustomHeadingId],
         rehypePlugins: [
