a0d4c994f00a4d03afd4979c0a34f9850a449c51
Obinna Ekwuno <obinnacodes@gmail.com>
2024-11-05 13:31:26 +0000

    csharp not dotnet

--- src/util/rehypeCodemirrorPlugin.mjs
+++ src/util/rehypeCodemirrorPlugin.mjs
@@ -26,7 +26,7 @@ const surrealqlParser = _surrealqlParser.configure({
 
 const parser = {
     bash: bashParser,
-    dotnet: StreamLanguage.define(csharp),
+    csharp: StreamLanguage.define(csharp),
     rs: rustParser,
     rust: rustParser,
     js: javascriptParser,
