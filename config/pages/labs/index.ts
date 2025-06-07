f8b4fae74881ee7ad1861fb87286d7d2696c813b
Julian Mills <julian.exodius@gmail.com>
2025-06-07 14:01:46 +0200

    update category images

--- config/pages/labs/index.ts
+++ config/pages/labs/index.ts
@@ -1,8 +1,25 @@
-import EmbedImage from '@assets/img/image/example/embed.png';
-import GraphImage from '@assets/img/image/example/graph.png';
-import MachineLearningImage from '@assets/img/image/example/machine-learning.png';
-import RelationalImage from '@assets/img/image/example/relational.png';
-import SearchImage from '@assets/img/image/example/search.png';
+import CiCdDarkImg from '@assets/img/labs-categories/ci-cd.png';
+import CiCdLightImg from '@assets/img/labs-categories/ci-cd.light.png';
+import DemosDarkImg from '@assets/img/labs-categories/demos.png';
+import DemosLightImg from '@assets/img/labs-categories/demos.light.png';
+import DeploymentToolsDarkImg from '@assets/img/labs-categories/deployment-tools.png';
+import DeploymentToolsLightImg from '@assets/img/labs-categories/deployment-tools.light.png';
+import DevelopmentToolsDarkImg from '@assets/img/labs-categories/development-tools.png';
+import DevelopmentToolsLightImg from '@assets/img/labs-categories/development-tools.light.png';
+import DockerImagesDarkImg from '@assets/img/labs-categories/docker-images.png';
+import DockerImagesLightImg from '@assets/img/labs-categories/docker-images.light.png';
+import IntegrationsDarkImg from '@assets/img/labs-categories/integrations.png';
+import IntegrationsLightImg from '@assets/img/labs-categories/integrations.light.png';
+import LibrariesDarkImg from '@assets/img/labs-categories/libraries.png';
+import LibrariesLightImg from '@assets/img/labs-categories/libraries.light.png';
+import SdksDarkImg from '@assets/img/labs-categories/sdks.png';
+import SdksLightImg from '@assets/img/labs-categories/sdks.light.png';
+import TemplatesDarkImg from '@assets/img/labs-categories/templates.png';
+import TemplatesLightImg from '@assets/img/labs-categories/templates.light.png';
+import TutorialsDarkImg from '@assets/img/labs-categories/tutorials.png';
+import TutorialsLightImg from '@assets/img/labs-categories/tutorials.light.png';
+import VideosDarkImg from '@assets/img/labs-categories/videos.png';
+import VideosLightImg from '@assets/img/labs-categories/videos.light.png';
 
 export const LAB_CATEGORIES = [
     'CI/CD',
@@ -47,16 +64,23 @@ export const LAB_SIDEBAR = [
 ];
 
 export const LAB_IMAGE_MAP = {
-    'CI/CD': EmbedImage,
-    'Deployment Tools': RelationalImage,
-    'Development Tools': SearchImage,
-    'Docker Images': GraphImage,
-    Integrations: MachineLearningImage,
-    Libraries: RelationalImage,
-    SDKs: SearchImage,
-    Templates: GraphImage,
-    Tutorials: MachineLearningImage,
-    Videos: EmbedImage,
+    'CI/CD': { light: CiCdLightImg, dark: CiCdDarkImg },
+    'Deployment Tools': {
+        light: DeploymentToolsLightImg,
+        dark: DeploymentToolsDarkImg,
+    },
+    'Development Tools': {
+        light: DevelopmentToolsLightImg,
+        dark: DevelopmentToolsDarkImg,
+    },
+    'Docker Images': { light: DockerImagesLightImg, dark: DockerImagesDarkImg },
+    Demos: { light: DemosLightImg, dark: DemosDarkImg },
+    Integrations: { light: IntegrationsLightImg, dark: IntegrationsDarkImg },
+    Libraries: { light: LibrariesLightImg, dark: LibrariesDarkImg },
+    SDKs: { light: SdksLightImg, dark: SdksDarkImg },
+    Templates: { light: TemplatesLightImg, dark: TemplatesDarkImg },
+    Tutorials: { light: TutorialsLightImg, dark: TutorialsDarkImg },
+    Videos: { light: VideosLightImg, dark: VideosDarkImg },
 } as const;
 
 export const LABS_TOPICS_MAP: Record<
