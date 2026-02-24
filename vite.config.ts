4a716c9a3076bbe021d88ed7d24525adb66f1d62
Beau den Heijer <beaudenheijer@gmail.com>
2026-02-24 17:49:10 +0100

    temp remove base property in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    base: "/docs",
     plugins: [
         vike(),
         react(),
