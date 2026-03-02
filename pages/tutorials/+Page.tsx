2d727589d2bf7fa8621cc73f61fdc50eef1b59fc
Beau den Heijer <beaudenheijer@gmail.com>
2026-03-02 16:06:49 +0100

    added tutorials pages

---
+++ pages/tutorials/+Page.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mantine/core";
+import { RenderMarkdown } from "@surrealdb/ui";
+import { useData } from "vike-react/useData";
+import type { TutorialsPageData } from "./+data";
+
+export default function Page() {
+    const { ast } = useData<TutorialsPageData>();
+    return (
+        <Box className="flag-markdown">
+            <RenderMarkdown ast={ast} />
+        </Box>
+    );
+}
