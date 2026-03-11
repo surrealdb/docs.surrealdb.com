7d8c8ae542059e334357b347afaa813a8d0753a8
Beau den Heijer <beaudenheijer@gmail.com>
2026-03-11 20:45:58 +0100

    enhance layout component with color scheme handling and improve markdown rendering in integrations page

--- pages/+Layout.tsx
+++ pages/+Layout.tsx
@@ -1,6 +1,6 @@
 // https://vike.dev/Layout
 
-import { MantineProvider } from "@mantine/core";
+import { Box, MantineProvider, useComputedColorScheme } from "@mantine/core";
 import { MANTINE_THEME } from "@surrealdb/ui";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -9,7 +9,22 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             theme={MANTINE_THEME}
             defaultColorScheme="auto"
         >
-            {children}
+            <FixColorScheme>{children}</FixColorScheme>
         </MantineProvider>
+    );
+}
+
+function FixColorScheme({ children }: { children: React.ReactNode }) {
+    const scheme = useComputedColorScheme();
+    return (
+        <>
+            <Box
+                display="none"
+                aria-hidden={true}
+            >
+                {scheme}
+            </Box>
+            {children}
+        </>
     );
 }
--- pages/integrations/+Page.tsx
+++ pages/integrations/+Page.tsx
@@ -1,11 +1,22 @@
 import { Box } from "@mantine/core";
-import { RenderMarkdown } from "@surrealdb/ui";
+import { RenderMarkdown, type Root } from "@surrealdb/ui";
 import { registerMarkdownComponents } from "@util/markdown";
 import { useData } from "vike-react/useData";
 import type { IntegrationsPageData } from "./+data";
 
+function _Render({ ast }: { ast: Root }) {
+    return (
+        <RenderMarkdown
+            ast={ast}
+            components={{ ...registerMarkdownComponents() }}
+            rendererProps={{ jsx: "render" }}
+        />
+    );
+}
+
 export default function Page() {
     const { ast } = useData<IntegrationsPageData>();
+
     return (
         <Box className="flag-markdown">
             <RenderMarkdown
