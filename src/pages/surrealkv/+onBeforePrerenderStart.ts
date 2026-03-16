ec077b333cd12b6a6549aa70c2b1ab9fe2c7772a
Beau den Heijer <beaudenheijer@gmail.com>
2026-03-16 12:58:00 +0100

    remove SurrealKV related files

--- src/pages/surrealkv/+onBeforePrerenderStart.ts
+++
@@ -1,7 +0,0 @@
-import { getCollection } from "vike-content-collection";
-
-export default function onBeforeRenderStart() {
-    return getCollection("doc-surrealkv").map((entry) =>
-        entry.slug === "index" ? "/surrealkv" : `/surrealkv/${entry.slug}`,
-    );
-}
