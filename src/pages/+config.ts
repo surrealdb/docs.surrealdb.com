2805242064f515de897d724ab1d685554aa449a9
Beau den Heijer <beaudenheijer@gmail.com>
2026-03-17 23:25:03 +0100

    Update vike-sitemap-generator integration and add sitemap configurations for multiple pages

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -2,20 +2,24 @@ import type { Config } from "vike/types";
 import vikeContentCollectionConfig from "vike-content-collection/config";
 import vikePhoton from "vike-photon/config";
 import vikeReact from "vike-react/config";
+import vikeSitemapConfig from "vike-sitemap-generator/config";
 import { DEFAULT_META_DESCRIPTION, DEFAULT_META_TITLE } from "~/utils/meta";
 
 export default {
     title: DEFAULT_META_TITLE,
     description: DEFAULT_META_DESCRIPTION,
-    extends: [vikeReact, vikeContentCollectionConfig, vikePhoton],
+    extends: [vikeReact, vikeContentCollectionConfig, vikePhoton, vikeSitemapConfig],
     prerender: true,
     passToClient: ["llms", "sdkVersions"],
     meta: {
         Page: {
-            env: { client: true, server: true },
+            env: { client: false, server: true },
         },
         data: {
             env: { server: true },
+        },
+        sitemap: {
+            env: { server: true, client: false },
         },
     },
 } satisfies Config;
---
+++ src/pages/index/+sitemap.ts
@@ -0,0 +1,6 @@
+import type { SitemapPageConfig } from "vike-sitemap-generator";
+
+export default {
+    priority: 1.0,
+    changefreq: "weekly",
+} satisfies SitemapPageConfig;
