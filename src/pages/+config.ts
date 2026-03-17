fa569e5000739fe41b4a343de40a14c5bfaa369e
Beau den Heijer <beaudenheijer@gmail.com>
2026-03-17 19:17:13 +0100

    apply linting

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -5,17 +5,17 @@ import vikeReact from "vike-react/config";
 import { DEFAULT_META_DESCRIPTION, DEFAULT_META_TITLE } from "~/utils/meta";
 
 export default {
-	title: DEFAULT_META_TITLE,
-	description: DEFAULT_META_DESCRIPTION,
-	extends: [vikeReact, vikeContentCollectionConfig, vikePhoton],
-	prerender: true,
-	passToClient: ["llms", "sdkVersions"],
-	meta: {
-		Page: {
-			env: { client: true, server: true },
-		},
-		data: {
-			env: { server: true },
-		},
-	},
+    title: DEFAULT_META_TITLE,
+    description: DEFAULT_META_DESCRIPTION,
+    extends: [vikeReact, vikeContentCollectionConfig, vikePhoton],
+    prerender: true,
+    passToClient: ["llms", "sdkVersions"],
+    meta: {
+        Page: {
+            env: { client: true, server: true },
+        },
+        data: {
+            env: { server: true },
+        },
+    },
 } satisfies Config;
