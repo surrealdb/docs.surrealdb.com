db4c2e400af81ca855e353549709c48bd64299de
Micha de Vries <micha@devrie.sh>
2026-03-24 10:16:08 +0100

    titles???

--- src/utils/markdown.tsx
+++ src/utils/markdown.tsx
@@ -10,10 +10,6 @@ import { resolveAstImages } from "./image-urls";
 export function resolveMarkdown(markdown: string) {
     const ast = parseMarkdown(markdown);
 
-    if (ast.children[0]?.type === "heading" && ast.children[0].depth === 1) {
-        ast.children = ast.children.slice(1);
-    }
-
     resolveAstImages(ast);
     const headings = extractHeadings(ast);
 
