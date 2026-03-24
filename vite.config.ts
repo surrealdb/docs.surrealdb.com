7c1c1c60b355d3b66f838570847158a7405018ff
Beau <beaudenheijer@gmail.com>
2026-03-24 12:27:12 +0100

    remove polyfills (#1682)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vike from "vike/plugin";
 import { getCollectionEntry, vikeContentCollectionPlugin } from "vike-content-collection";
 import { getLastModFromGit, vikeSitemap } from "vike-sitemap-generator";
 import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { docs, sdks, versionedSdks } from "./src/content/config";
 
@@ -46,9 +45,6 @@ export default defineConfig({
     plugins: [
         vike(),
         react(),
-        nodePolyfills({
-            include: ["buffer"],
-        }),
         tsconfigPaths(),
         vikeContentCollectionPlugin({
             contentDir: "src/content",
@@ -60,8 +56,6 @@ export default defineConfig({
         }),
         vikeSitemap({
             baseUrl: "https://surrealdb.com/docs",
-            // outDir: "./.vercel/output/static",
-            // outFile: "sitemap.xml",
             robots: true,
             priority: (url, { urls }) => {
                 if (url === "/") return 1.0;
@@ -106,12 +100,6 @@ export default defineConfig({
         sourcemap: true,
         minify: true,
         cssMinify: true,
-        rollupOptions: {
-            external: [
-                "vite-plugin-node-polyfills/shims/process",
-                "vite-plugin-node-polyfills/shims/global",
-            ],
-        },
     },
     ssr: {
         noExternal: ["@surrealdb/ui", "@mantine/core", "@mantine/hooks", "@mantine/spotlight"],
