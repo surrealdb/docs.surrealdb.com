5caca0136c5ac0713af5bb4a709bace997aa8974
Beau den Heijer <beaudenheijer@gmail.com>
2026-04-15 23:07:55 +0200

    progress

---
+++ src/utils/labs.ts
@@ -0,0 +1,28 @@
+export const labCategories = [
+    "Code repositories",
+    "Videos",
+    "Blogposts",
+    "Documentation",
+    "Learning Resources",
+] as const;
+
+export const labTopics = [
+    "AI",
+    "Cloud",
+    "Data Management",
+    "Examples",
+    "Libraries",
+    "Security",
+    "Templates",
+    "Tooling",
+] as const;
+
+export const labLanguages = [
+    "Python",
+    "Rust",
+    "TypeScript",
+    "Go",
+    "Java",
+    "PHP",
+    "SurrealQL",
+] as const;
