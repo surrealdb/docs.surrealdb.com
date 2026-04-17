4e2b3a98d1298327e1a8b764abf9edf4361fc01a
Julian Mills <julian.exodius@gmail.com>
2026-04-17 17:17:41 +0200

    disable check for now

--- scripts/index-search.ts
+++ scripts/index-search.ts
@@ -10,10 +10,10 @@ import { runIndexer } from "../search/scripts/indexer";
 const isProduction = process.env.VERCEL_ENV === "production";
 const isVercel = !!process.env.VERCEL;
 
-if (isVercel && !isProduction) {
-    console.log("[IX] Skipping search indexing (non-production Vercel build)");
-    process.exit(0);
-}
+// if (isVercel && !isProduction) {
+//     console.log("[IX] Skipping search indexing (non-production Vercel build)");
+//     process.exit(0);
+// }
 
 runIndexer().catch((err) => {
     console.error("[FATAL]", err);
