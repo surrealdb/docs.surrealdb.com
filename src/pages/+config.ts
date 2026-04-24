404f887c38633f143aefb5ad8c0368db7731a81f
Beau den Heijer <beaudenheijer@gmail.com>
2026-04-24 11:39:53 +0200

    apply linting

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -4,28 +4,29 @@ import vikeReact from "vike-react/config";
 import vikeSitemapConfig from "vike-sitemap-generator/config";
 
 export default {
-	title: "SurrealDB | The ultimate multi-model database for tomorrow's applications",
-	description: "SurrealDB is the ultimate database for tomorrow's serverless, jamstack, single-page, and traditional applications.",
-	extends: [vikeReact, vikeContentCollectionConfig, vikeSitemapConfig],
-	prerender: false,
-	passToClient: ["sdkVersions"],
-	server: true,
-	ssr: true,
-	meta: {
-		Page: {
-			env: { server: true, client: true },
-		},
-		data: {
-			env: { server: true, client: false },
-		},
-		sitemap: {
-			env: { config: true, server: true, client: false },
-		},
-		sitemapUrls: {
-			env: { config: true, server: true },
-		},
-		Content: {
-			env: { server: true, client: false },
-		}
-	},
+    title: "SurrealDB | The ultimate multi-model database for tomorrow's applications",
+    description:
+        "SurrealDB is the ultimate database for tomorrow's serverless, jamstack, single-page, and traditional applications.",
+    extends: [vikeReact, vikeContentCollectionConfig, vikeSitemapConfig],
+    prerender: false,
+    passToClient: ["sdkVersions"],
+    server: true,
+    ssr: true,
+    meta: {
+        Page: {
+            env: { server: true, client: true },
+        },
+        data: {
+            env: { server: true, client: false },
+        },
+        sitemap: {
+            env: { config: true, server: true, client: false },
+        },
+        sitemapUrls: {
+            env: { config: true, server: true },
+        },
+        Content: {
+            env: { server: true, client: false },
+        },
+    },
 } satisfies Config;
